ec0d03db0fa5c4330f1e24e010f4717fa801a478
Matthew Andre Tuason <m.tuason969@mybvc.ca>
2024-09-20 13:49:11 -0600

    first commit

---
+++ evenorodd.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num int
+
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&num)
+
+	if num%2 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+}
